71a4c6d2ce4651c77b66deb3f7488937c3ecf300
Sergey <sergeychikin1981@gmail.com>
2023-05-09 15:37:02 +0300

    First commit.

---
+++ src/test/java/pro/sky/calculatorwithtests/CalculatorWithTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorwithtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorWithTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
